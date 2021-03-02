8eb407a2b48abf3a05250fe6672c209b1d747579
Cailin Young <cailinfayyoung@gmail.com>
2021-03-01 21:03:43 -0700

    push for help

--- db/schema.sql
+++ db/schema.sql
@@ -6,29 +6,71 @@ USE employee_trackerDB;
 
 -- department table --
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    department VARCHAR(30) NOT NULL,
-    primary key (id)
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) 
+        
 );
 
 -- role table --
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    primary key (id),
-    foreign key (department_id) references department(id)
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT(10),
+    FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+    
 );
 
 -- employee table -- 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NULL,
-    primary key (id),
-    foreign key (role_id) references role(id),
-    foreign key (manager_id) references employee(id)
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT(10),
+    manager_id INT(10), 
+    FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee (employee_id) ON DELETE SET NULL
+
 );
+
+-- role seeds (From GIF) --
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    
+    --   department seeds (From GIF)--
+    INSERT INTO department (department)
+    VALUES
+        ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+    
+    -- role seeds (From GIF) --
+    INSERT INTO role (title, salary, department_id)
+    VALUES
+        ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4),
+        ("Lawyer", 190000, 4);
+    
+    -- employee seeds (From GIF) --
+    INSERT INTO employee (first_name, last_name, role_id)
+    VALUES
+        ("John", "Doe", 1),
+        ("Mike", "Chan", 2),
+        ("Ashley", "Rodriguez", 3),
+        ("Kevin", "Tupik", 4),
+        ("Jane", "Smith", 5),
+        ("Malia", "Brown", 5),
+        ("Sarah", "Lourd", 6),
+        ("Tom", "Allen", 7),
+        ("Christian", "Eckenrode", 3),
+        ("Tammer", "Galal", 4);
+    
--- db/server.js
+++ db/server.js
@@ -3,14 +3,16 @@ const mysql = require("mysql");
 
 // setting up localhost connection
 const connection = mysql.createConnection({
-    host: "local connection",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "employee_trackerDB"
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: 'password',
+    database: 'employee_trackerDB'
 });
 
-connection.connect();
+connection.connect()
+
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
 
