59d480b47c353d26a1fdd7e22cccc9c63db0f410
Cailin Young <cailinfayyoung@gmail.com>
2021-03-01 22:16:46 -0700

    fixing mysql errors

--- schema.sql
+++ schema.sql
@@ -4,21 +4,19 @@ DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_trackerDB;
 
--- department table --
+-- creating department table --
 CREATE TABLE department (
     department_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) 
-        
+    name VARCHAR(30)       
 );
 
--- role table --
+-- creating role table --
 CREATE TABLE role (
     role_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT(10),
-    FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
-    
+    FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE   
 );
 
 -- employee table -- 
@@ -30,8 +28,8 @@ CREATE TABLE employee (
     manager_id INT(10), 
     FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE,
     FOREIGN KEY (manager_id) REFERENCES employee (employee_id) ON DELETE SET NULL
-
 );
+
 
 -- role seeds (From GIF) --
 INSERT INTO role (title, salary, department_id)
@@ -49,28 +47,27 @@ VALUES
         ("Finance"),
         ("Legal");
     
-    -- role seeds (From GIF) --
-    INSERT INTO role (title, salary, department_id)
-    VALUES
-        ("Sales Lead", 100000, 1),
-        ("Salesperson", 80000, 1),
-        ("Lead Engineer", 150000, 2),
-        ("Software Engineer", 120000, 2),
-        ("Accountant", 125000, 3),
-        ("Legal Team Lead", 250000, 4),
-        ("Lawyer", 190000, 4);
+-- role seeds (From GIF) --
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000, 2),
+    ("Accountant", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4);
     
-    -- employee seeds (From GIF) --
-    INSERT INTO employee (first_name, last_name, role_id)
-    VALUES
-        ("John", "Doe", 1),
-        ("Mike", "Chan", 2),
-        ("Ashley", "Rodriguez", 3),
-        ("Kevin", "Tupik", 4),
-        ("Jane", "Smith", 5),
-        ("Malia", "Brown", 5),
-        ("Sarah", "Lourd", 6),
-        ("Tom", "Allen", 7),
-        ("Christian", "Eckenrode", 3),
-        ("Tammer", "Galal", 4);
+-- employee seeds (From GIF) --
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+    ("John", "Doe", 1, NULL),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, 1),
+    ("Kevin", "Tupik", 4, 1),
+    ("Jane", "Smith", 5, 1),
+    ("Malia", "Brown", 5, 1),
+    ("Sarah", "Lourd", 6, 1),
+    ("Tom", "Allen", 7, 1),
+    ("Christian", "Eckenrode", 3, 1);
     
